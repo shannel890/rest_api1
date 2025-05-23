e0b658c69631c2f28d347ae4a2c01ae049e3968b
shannel890 <shannelkirui739@gmail.com>
2025-05-23 15:58:09 +0300

    modified  app/__init__.py

--- school_management_system/app/__init__.py
+++ school_management_system/app/__init__.py
@@ -3,7 +3,10 @@ from app.extension import db
 from flask_restful import Api
 from app.resources.user import Users,User
 from app.resources.teacher import Teachers, Teacher
-
+from app.resources.student import Students,Student
+from app.resources.course import Courses,Course
+from app.resources.fee  import Fees,Fee
+from app.resources.enrolment import Enrolments,Enrolment
 from config import Config
 from flask_migrate import Migrate
 
@@ -17,10 +20,18 @@ api = Api(app)
 migrate = Migrate(app,db)
 
  #api endpoints
-api.add_resource(Users,'/api/users/')
+api.add_resource(Users,'/api/users')
 api.add_resource(User,'/api/users/<int:id>')
 api.add_resource(Teachers,'/api/teachers')
 api.add_resource(Teacher,'/api/teachers/<int:id>')
+api.add_resource(Students,'/api/students')
+api.add_resource(Student,'/api/students/<int:id>')
+api.add_resource(Courses,'/api/courses')
+api.add_resource(Course,'/api/courses/<int:id>')
+api.add_resource(Fees,'/api/fees')
+api.add_resource(Fee,'/api/fees/<int:id>')
+api.add_resource(Enrolments,'/api/enrolments')
+api.add_resource(Enrolment,'/api/enrolments/<int:id>')
 
 
          
